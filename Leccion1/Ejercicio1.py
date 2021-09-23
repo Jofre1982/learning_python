bffc7f26602b5f25b5169a7da749dafb81bc02dc
Jofre <jofre1706@gmail.com>
2021-09-23 17:41:36 -0500

    Mi nprimer ejercicio python

---
+++ Leccion1/Ejercicio1.py
@@ -0,0 +1,6 @@
+print("Calcular cripton\n")
+cripto=input("Ingrese su criptomoneda: ")
+valor=int(input("Valor actual en dolares de su cripto: "))
+monto=int(input("Cuanto tiene guardado: "))
+total=valor*monto
+print ("Usted posee un total de US$ "+ str(total))
